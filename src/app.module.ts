39d1ba718204ac4be22a031115977efbf22e6a37
Nils Effinghausen <nils.effinghausen@maibornwolff.de>
2021-09-28 18:28:12 +0200

    add nestjs-rate-limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { SyncStateController } from './sync-state/sync-state.controller';
 import { SyncStateService } from './sync-state/sync-state.service';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { RateLimiterGuard, RateLimiterModule } from 'nestjs-rate-limiter';
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), RateLimiterModule],
   controllers: [SyncStateController],
-  providers: [SyncStateService],
+  providers: [
+    SyncStateService,
+    {
+      provide: APP_GUARD,
+      useClass: RateLimiterGuard,
+    },
+  ],
 })
 export class AppModule {}
